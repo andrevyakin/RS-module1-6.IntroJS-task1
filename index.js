db81ec3f61b0b77c100ec34edb65df4e93863411
Андрей <andrevyakin@gmail.com>
2022-11-08 21:43:11 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('' ?? 'Done');
-console.log('' || 'Done');
+const existingUserLogin = "the_best_user";
+const existingUserPassword = "12345678";
+
+const userLogin = prompt("Введите логин").trim();
+const userPassword = prompt("Введите пароль").trim();
+
+if(userLogin===existingUserLogin&&userPassword===existingUserPassword)
+    alert(`Добро пожаловать, ${userLogin}!`);
+else
+    alert("Логин и (или) Пароль введены неверно!");
